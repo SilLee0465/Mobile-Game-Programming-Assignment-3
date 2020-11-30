b5739ca44a6fa72200568f6d9925286af8a028bb
SilLee <61728125+SilLee0465@users.noreply.github.com>
2020-11-30 23:01:14 +0800

    Added some stuff
    Added:
    camera follow

--- Mobile Game Programming Assignment 3/Assets/Scripts/LeftRightInput.cs
+++ Mobile Game Programming Assignment 3/Assets/Scripts/LeftRightInput.cs
@@ -6,16 +6,29 @@ public class LeftRightInput : MonoBehaviour
 {
     [SerializeField] private LerpFoward LerpFoward;
     public bool gameStart, isGameOver = false;
+    private float speedModifier;
+    private Touch touch;
+
+    void Start()
+    {
+        speedModifier = 0.01f;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.touchCount > 0 && !isGameOver)
         {
-            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            gameStart = true;
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
             {
-                gameStart = true;
+                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speedModifier, transform.position.y, transform.position.z);
             }
+            //if (Input.GetTouch(0).phase == TouchPhase.Began)
+            //{
+            //    gameStart = true;
+            //}
         }
     }
 
---
+++ Mobile Game Programming Assignment 3/Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(player.position.x, 4.45f, player.position.z - 10.53f);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
